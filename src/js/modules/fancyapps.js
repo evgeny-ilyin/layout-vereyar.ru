5263b785856bf52cc9457e1c6fdbc81c2de4e801
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2024-07-26 16:19:53 +0300

    fancyapp styling

--- src/js/modules/fancyapps.js
+++ src/js/modules/fancyapps.js
@@ -1,23 +1,16 @@
 import { Fancybox } from "@fancyapps/ui";
-import { Carousel } from "@fancyapps/ui/dist/carousel/carousel.esm.js";
+// import { Carousel } from "@fancyapps/ui/dist/carousel/carousel.esm.js";
 
 addEventListener("DOMContentLoaded", () => {
 	// Модальные окна
 	Fancybox.bind("[data-fancybox]", {});
 
-	// Карусели
-	Carousel.defaults = {
-		...Carousel.defaults,
-		infinite: false,
-		adaptiveHeight: true,
-	};
-
 	// Карусели в модальных окнах
-	const modalCarousels = document.querySelectorAll(".modal_carousel .f-carousel");
-	modalCarousels.forEach((el) => {
-		let carousel = document.getElementById(el.id);
-		if (carousel) {
-			new Carousel(carousel);
-		}
-	});
+	// const modalCarousels = document.querySelectorAll(".modal_carousel .f-carousel");
+	// modalCarousels.forEach((el) => {
+	// 	let carousel = document.getElementById(el.id);
+	// 	if (carousel) {
+	// 		new Carousel(carousel);
+	// 	}
+	// });
 });
